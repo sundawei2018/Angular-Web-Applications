a0e7e34063f4fc41b1f9d96bc782b7133c736df1
Dawei Sun <dsun2016@gmail.com>
2017-03-31 21:46:37 -0400

    finished part 2

--- project-part1/src/app/app.component.ts
+++ project-part1/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  loadedFeature = 'recipe';
+
+  onNavigated(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
--- project-part1/src/app/recipes/recipe-list/recipe-list.component.ts
+++ project-part1/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,EventEmitter,Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
@@ -6,13 +6,18 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('A test', 'this is a test','https://media.licdn.com/mpr/mpr/AAEAAQAAAAAAAAbWAAAAJDhmOTYyNmJmLTBlYjMtNGQ1OC05YTgxLTgwODI5MjE1YTQ2NA.png')
+    new Recipe('A test', 'this is a test','https://media.licdn.com/mpr/mpr/AAEAAQAAAAAAAAbWAAAAJDhmOTYyNmJmLTBlYjMtNGQ1OC05YTgxLTgwODI5MjE1YTQ2NA.png'),
+    new Recipe('B test', 'this is another test','http://bloximages.newyork1.vip.townnews.com/stltoday.com/content/tncms/assets/v3/editorial/a/3a/a3ac2583-4bf3-5a43-b71a-8ae061279d43/555de70049716.image.gif')
   ];
-  
+
   constructor() { }
 
   ngOnInit() {
+  }
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
