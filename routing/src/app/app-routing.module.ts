bea5a1defa52612f546e02bb41096b3561c0944e
Your Name <you@example.com>
2017-05-24 22:57:46 -0400

    new changes

--- routing/src/app/app-routing.module.ts
+++ routing/src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AuthGuard } from './auth-guard.service';
-
+import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -17,12 +17,11 @@ const appRoutes: Routes = [
     { path: ':id/:name', component: UserComponent }
   ] },
   { path: 'servers', 
-  // canActivate: [AuthGuard] ,
-  canActivateChild: [AuthGuard],
-  component: ServersComponent, 
-  children: [
+    canActivateChild: [AuthGuard],
+    component: ServersComponent, 
+    children: [
     { path: ':id', component: ServerComponent },
-    { path: ':id/edit', component: EditServerComponent },
+    { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] }
   ] },
   { path: 'not-found', component: PageNotFoundComponent },
   { path: '**', redirectTo: '/not-found' }
