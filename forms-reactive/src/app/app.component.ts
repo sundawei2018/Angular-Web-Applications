27ff4ec190cb968c1eec7281ba86729a0a14d007
Your Name <you@example.com>
2017-05-29 19:51:51 -0400

    finished forms-react

--- forms-reactive/src/app/app.component.ts
+++ forms-reactive/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -14,15 +16,35 @@ export class AppComponent implements OnInit {
     this.signupForm = new FormGroup({
       'userData' : new FormGroup({
         'username' : new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-        'email' : new FormControl(null, [Validators.required, Validators.email]),
+        'email' : new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails),
       }),
       'gender' : new FormControl('male'),
       'hobbies' : new FormArray([])
+    });
+    // this.signupForm.valueChanges.subscribe(
+    //   (value) => console.log(value)
+    // );
+    this.signupForm.statusChanges.subscribe(
+      (value) => console.log(value)
+    );
+    this.signupForm.setValue({
+      'userData' : {
+        'username' : 'Max',
+        'email' : 'max@gmail.com'
+      },
+      'gender' : 'male',
+      'hobbies' : []
+    });
+    this.signupForm.patchValue({
+      'userData' : {
+        'username' : 'Anna',
+      }
     });
   }
 
   onSubmit() {
     console.log(this.signupForm);
+    this.signupForm.reset();
   }
 
   onAddHobby() {
@@ -35,5 +57,18 @@ export class AppComponent implements OnInit {
       return { 'nameIsForbidden' : true };
     }
     return null;
+  }
+
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({'emailIsForbidden' : true});
+        } else {
+          resolve(null);
+        }
+      },1500);
+    });
+    return promise;
   }
 }
