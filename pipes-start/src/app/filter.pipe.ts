8d025a94910e2ee4a0313cb53c7ae1d6213967f3
Your Name <you@example.com>
2017-08-31 15:11:11 -0400

    new changes

--- pipes-start/src/app/filter.pipe.ts
+++ pipes-start/src/app/filter.pipe.ts
@@ -1,20 +1,21 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'filter'
+  name: 'filter',
+  pure: false
 })
 export class FilterPipe implements PipeTransform {
 
   transform(value: any, filterString: string, propName: string): any {
-    if (value.length == 0) {
+    if (value.length == 0 || filterString  === '') {
       return value;
     }
+    const resultArray = [];
     for (const item of value) {
-      const resultArray = [];
       if (item[propName] === filterString) {
         resultArray.push(item);
       }
-      return resultArray;
     }
+    return resultArray;
   }
 }
